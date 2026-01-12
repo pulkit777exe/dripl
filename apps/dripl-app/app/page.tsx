deb37e2a167b451369bf799aab7381b9e5bf0746
pulkit777exe <pulkitpareek88@gmail.com>
2026-01-13 00:11:22 +0530

    added new landing page

--- apps/dripl-app/app/page.tsx
+++ apps/dripl-app/app/page.tsx
@@ -1,5 +1,20 @@
-import { LandingPage } from "@/components/home/LandingPage";
+import { MarketingLayout } from "@/components/marketing/MarketingLayout";
+import { HeroSection } from "@/components/marketing/sections/HeroSection";
+import { BentoGrid } from "@/components/marketing/sections/BentoGrid";
+import { IntegrationCarousel } from "@/components/marketing/sections/IntegrationCarousel";
+import { WorkflowSection } from "@/components/marketing/sections/WorkflowSection";
+import { TestimonialCarousel } from "@/components/marketing/sections/TestimonialCarousel";
+import { PricingTable } from "@/components/marketing/sections/PricingTable";
 
 export default function Page() {
-  return <LandingPage />;
+  return (
+    <MarketingLayout>
+      <HeroSection />
+      <BentoGrid />
+      <IntegrationCarousel />
+      <WorkflowSection />
+      <TestimonialCarousel />
+      <PricingTable />
+    </MarketingLayout>
+  );
 }
