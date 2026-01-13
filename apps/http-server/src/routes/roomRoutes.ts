f4483db9ae61c67fb4c496fd62c1128257b2502b
pulkit777exe <pulkitpareek88@gmail.com>
2026-01-13 20:13:10 +0530

    removed comments

--- apps/http-server/src/routes/roomRoutes.ts
+++ apps/http-server/src/routes/roomRoutes.ts
@@ -4,17 +4,15 @@ import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router: Router = Router();
 
-// All routes require authentication
 router.use(authMiddleware);
 
-// Room CRUD
 router.get("/", RoomController.getRooms);
 router.post("/", RoomController.createRoom);
 router.get("/:slug", RoomController.getRoom);
 router.put("/:slug", RoomController.updateRoom);
 router.delete("/:slug", RoomController.deleteRoom);
 
-// Room member management
+// management
 router.post("/:slug/members", RoomController.addMember);
 router.delete("/:slug/members/:userId", RoomController.removeMember);
 
